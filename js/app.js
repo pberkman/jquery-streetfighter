4d1e626de65c94e8458c8dd2070d6c45a6ba5e71
pberkman <patriciaberkman@gmail.com>
2014-05-26 15:03:38 -0400

    js assignment 7pt2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-  $(".ryu").mouseenter(function(){
+$(document).ready(function() {
+  $(".ryu").mouseenter(function(){ 
   	$(".ryu-still").hide();
   	$(".ryu-ready").show();
   })
@@ -7,16 +7,29 @@ $(document).ready(function(){
   	$(".ryu-ready").hide();
   	$(".ryu-still").show();
   })
-  .mousedown(function(){
+  .mousedown(function() {
+  	playHadouken();
   	$(".ryu-ready").hide();
   	$(".ryu-throwing").show();
-  	$(".hadouken").show();
-  	// play hadouken sound
-  	//show hadouken and animate it to the right of the screen
-  })
-  .mouseup(function(){
+  	$(".hadouken").finish().show()
+	.animate(
+  		{"left":"300px"}, 
+  		500,
+  		function() {
+  			$(this).hide();
+  			$(this).css("left","-212px");
+  		}
+  	  );
+  	})
+  .mouseup(function() {
   	$(".ryu-throwing").hide();
   	$(".ryu-ready").show();
   	//ryu goes back to his ready position
-  })
+  });
 });
+function playHadouken () {
+	$("#hadouken-sound")[0].volume = 0.5;
+	$("#hadouken-sound")[0].load();
+	$("#hadouken-sound")[0].play();
+
+}
