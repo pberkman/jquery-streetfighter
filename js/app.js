0bced61fb3cc965f7674c395a739c99bbe596338
Kevin Lamping <klamping@gmail.com>
2014-06-03 19:18:09 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,43 +1,53 @@
 $(document).ready(function() {
+  var isPosing = false;
   $(".ryu").mouseenter(function(){ 
-  	$(".ryu-still").hide();
-  	$(".ryu-ready").show();
+  	if (isPosing == false) {
+  		$(".ryu-still").hide();
+  		$(".ryu-ready").show();
+  	}
   })
   .mouseleave(function() {
-  	$(".ryu-ready").hide();
-  	$(".ryu-still").show();
+  	if (!isPosing) {
+  		$(".ryu-ready").hide();
+  		$(".ryu-still").show();
+  	}
   })
   .mousedown(function() {
-  	playHadouken();
-  	$(".ryu-ready").hide();
-  	$(".ryu-throwing").show();
-  	$(".hadouken").finish().show()
-	.animate(
-  		{"left":"300px"}, 
-  		500,
-  		function() {
-  			$(this).hide();
-  			$(this).css("left","-212px");
-  		}
-  	  );
+  	if (!isPosing) {
+	  	playHadouken();
+	  	$(".ryu-ready").hide();
+	  	$(".ryu-throwing").show();
+	  	$(".hadouken").finish().show()
+		.animate(
+	  		{"left":"300px"}, 
+	  		500,
+	  		function() {
+	  			$(this).hide();
+	  			$(this).css("left","-212px");
+	  		}
+	  	  );
+  	}
   	})
   .mouseup(function() {
-  	$(".ryu-throwing").hide();
-  	$(".ryu-ready").show();
-  	//ryu goes back to his ready position
+  	if (!isPosing) {
+	  	$(".ryu-throwing").hide();
+	  	$(".ryu-ready").show();
+  		//ryu goes back to his ready position
+  	}
   });
   
   $("body").keydown(function(){
   	if(event.which==88) {
-  	$(".ryu-still").hide();
-  	$(".ryu-ready").hide();
-  	$(".ryu-cool").show();
-  }
+	  	isPosing = true;
+	  	$(".ryu-still").hide();
+	  	$(".ryu-ready").hide();
+	  	$(".ryu-cool").show();
+	  }
   })
   .keyup(function() {
   	$(".ryu-cool").hide();
   	$(".ryu-still").show();
-
+	isPosing = false;
   })
 });
 function playHadouken () {
